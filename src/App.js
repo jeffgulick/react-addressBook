b6c799cbf8cfd513c539b8fca46c45185ee821bf
JEFFGULICK <jeffgulick@gmail.com>
2020-11-22 01:54:04 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import './App.css';
+import { Component } from 'react';
+import Display from './Components/display';
+
+export default class App extends Component{
+  constructor(){
+    super()
+
+    this.state ={
+      contacts: []
+    }
+  }
+  componentDidMount = () => {
+    fetch('https://randomuser.me/api?results=25')
+      .then(response => response.json())
+      .then(parsedJSON => parsedJSON.results.map(user =>(
+        {
+          name: `${user.name.first}, ${user.name.last}`,
+          thumbnail: `${user.picture.large}`,
+          phone: `${user.phone}, ${user.cell}`,
+          email: `${user.email}`,
+          id: `${user.login.uuid}`
+        }
+      
+      )))
+      .then(contacts => this.setState({
+        contacts,
+      }))
+  }
+
+  onClick = () => {
+    this.setState({
+      isClicked: !this.isClicked
+    })
+  }
+
+  render(){
+    return(
+      <div>
+        {this.state.contacts.map(user => (
+          <Display 
+            key={user.id}
+            user = {user} />
+        ))}
+      </div>
+    )
+  }
+}
---
+++ src/Components/display.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const Display = (props) => {
+    const [clicked, setClicked] = useState(false)
+
+    if(!clicked){
+        return(
+            <Container fluid className="p-1">
+                <Card style={{ width: '14rem', backgroundColor:"#D1ECF1" }}>
+                    <Card.Img variant="top" src={props.user.thumbnail} />
+                    <Card.Body>
+                        <Card.Title>{props.user.name}</Card.Title>
+                        <Button variant="primary" onClick={()=>setClicked(clicked=>!clicked)}>More Info</Button>
+                    </Card.Body>
+                </Card>
+
+            </Container>
+        )
+    } 
+    else {
+        return(
+            <Card style={{ width: '14rem', backgroundColor:"#D1ECF1" }}>
+                <Card.Img variant="top" src={props.user.thumbnail} />
+                <Card.Body>
+                    <Card.Title>{props.user.name}</Card.Title>
+                    <Card.Text>
+                        <p><span>Phone:</span> {props.user.phone}</p>
+                        <p><span>Email:</span> {props.user.email}</p>
+                    </Card.Text>
+                    <Button variant="primary" onClick={()=>setClicked(clicked=>!clicked)}>Less Info</Button>
+                </Card.Body>
+            </Card>
+
+        )
+    }
+}
+
+export default Display
